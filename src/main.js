93de11c8e3ad89df89d1444b40cde245e2d1c549
mclick <mclick@ucsc.edu>
2021-07-11 14:06:02 -0700

    enabled arcade physics

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ let config = {
     type: Phaser.AUTO,
     width: 640,
     height: 480,
-    scene: [ Menu, Play ]
+    scene: [ Menu, Play ],
+    physics:{
+        default: 'arcade',
+        arcade: {
+            gravity:{y: 1000},
+            debug: true, //set to false before release
+        }
+    }
 }
 let game = new Phaser.Game(config);
 // set UI sizes
