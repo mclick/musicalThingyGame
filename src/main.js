78f0d2db943b18572111bd77d047bf2ec5fcd99c
mclick <mclick@ucsc.edu>
2021-07-12 11:16:51 -0700

    basic physics - needs tweaking

--- src/main.js
+++ src/main.js
@@ -15,3 +15,5 @@ let game = new Phaser.Game(config);
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+let keyJump, keyLeft, keyRight;
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -5,7 +5,7 @@ class Play extends Phaser.Scene {
     preload(){
         this.load.image('tiles', 'assets/tempTileSet.png');
         this.load.tilemapTiledJSON('map', 'assets/tempTileMap.json');
-        this.load.image('plaer', 'assets/tempPlayer.png');
+        this.load.image('player', 'assets/tempPlayer.png');
     }
     create(){
         const map = this.make.tilemap({ key: 'map' });
@@ -13,12 +13,25 @@ class Play extends Phaser.Scene {
         const platforms = map.createStaticLayer('layer1', tileset, 0, 0);
 
         this.player = this.physics.add.sprite(50, 300, 'player');
-        this.player.setBounce(0.1);
+        this.player.setBounce(0.5);
         this.player.setCollideWorldBounds(true);
+        this.player.setDragX(200);
         this.physics.add.collider(this.player, platforms);
         platforms.setCollisionByExclusion(-1, true);
+
+        keyJump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
     update(){
-
+        if(keyLeft.isDown){
+            this.player.setVelocityX(-200);
+        }
+        if(keyRight.isDown){
+            this.player.setVelocityX(200);
+        }
+        if(Phaser.Input.Keyboard.JustDown(keyJump)){
+            this.player.setVelocityY(-400);
+        }
     }
 }
