9893b6348ef18fdc2bbbf2b0f8a2421a3e1f760f
mclick <mclick@ucsc.edu>
2021-07-12 11:03:59 -0700

    very basic collision stuff

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -5,11 +5,18 @@ class Play extends Phaser.Scene {
     preload(){
         this.load.image('tiles', 'assets/tempTileSet.png');
         this.load.tilemapTiledJSON('map', 'assets/tempTileMap.json');
+        this.load.image('plaer', 'assets/tempPlayer.png');
     }
     create(){
         const map = this.make.tilemap({ key: 'map' });
         const tileset = map.addTilesetImage('tempTileSet', 'tiles');
         const platforms = map.createStaticLayer('layer1', tileset, 0, 0);
+
+        this.player = this.physics.add.sprite(50, 300, 'player');
+        this.player.setBounce(0.1);
+        this.player.setCollideWorldBounds(true);
+        this.physics.add.collider(this.player, platforms);
+        platforms.setCollisionByExclusion(-1, true);
     }
     update(){
 
