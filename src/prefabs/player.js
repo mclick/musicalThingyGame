d688896aff468fa499a8d56116addfe63536551c
mclick <mclick@ucsc.edu>
2021-07-19 13:36:20 -0700

    deleting unused files

--- src/prefabs/player.js
+++
@@ -1,7 +0,0 @@
-class Player extends Phaser.GameObjects.Sprite {
-    constructor(scene,x,y,texture,frame){
-        super(scene,x,y,texture,frame);
-        scene.add.existing(this);
-    }
-    update()
-}
