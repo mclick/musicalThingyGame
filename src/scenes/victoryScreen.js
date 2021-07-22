89380b81dea578c0ed330b9b47f1ee4e52adaa0e
Stan <baconrulz101@gmail.com>
2021-07-21 23:57:11 -0700

    victory screen

--- src/scenes/victoryScreen.js
+++ src/scenes/victoryScreen.js
@@ -4,26 +4,14 @@ class Victory extends Phaser.Scene {
     }
 
     preload(){
+        this.load.image('vic','assets/victory.png');
     }
 
     create(){
 
+        this.menuScreen = this.add.sprite(400,300,'vic');
         keyESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
         keyC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
-        this.victoryConfig = {
-            fontFamily: 'Courier',
-            fontSize: '24px',
-            align: 'right',
-            padding: {
-              top: 5,
-              bottom: 5,
-            },
-            fixedWidth: 0
-        }
-
-        this.add.text(400,300,'You Won!', this.victoryConfig).setOrigin(0.5);
-        this.add.text(400,332,'Press Escape to return to the Menu', this.victoryConfig).setOrigin(0.5);
-        this.add.text(400,364,'Or Press C to see the Credits', this.victoryConfig).setOrigin(0.5);
     }
     update(){
         if(Phaser.Input.Keyboard.JustDown(keyESC)){
